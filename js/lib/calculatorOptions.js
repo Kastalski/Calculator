9e01994eb9c0adea9fb6f20a232ec0757d34f624
kastalski <S.Kastalski@software.travel>
2013-02-13 16:36:32 +0300

    Rejected commit: Default

---
+++ js/lib/calculatorOptions.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: kastalski
+ * Date: 13/02/13
+ * Time: 15:43
+ * To change this template use File | Settings | File Templates.
+ */
+define({
+    creationMethods: creationMethods,
+    helperMethods: helperMethods
+})
